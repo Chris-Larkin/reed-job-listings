bf2c71e4956265bf2389df30a03afc97f22c2c21
Chris-Larkin <Chris-Larkin@users.noreply.github.com>
2019-11-30 13:25:17 -0500

    scraper.py
    This code scrapes all job listings posted on reed.co.uk for a given day. It extracts: 
    
    applications_ten -> whether there have been ten or fewer applicants
    job_country -> country where the job is located
    job_description -> free text description of the job provided by the poster
    job_locality -> city/town where the job is located
    job_postcode -> postcode for where the job is located
    job_region -> region where the job is located
    job_type -> full-time, part-time, temporary, contract, etc.
    job_type_disp -> job type as displayed to applicants
    link -> URL to the job listing
    salary_disp -> salary as displayed to the applicant
    salary_max -> maximum salary
    salary_min -> minimum salary
    salary_time -> time unit over which salary is reported (hourly, monthly, yearly)
    
    It outputs a pandas data frame called reed_data.

---
+++ scraper.py
@@ -0,0 +1,83 @@
+# Import packages
+
+import requests
+import re
+import os
+import lxml.html
+import pandas as pd
+pd.set_option('max_colwidth',120)
+pd.set_option('display.max_row', 100)
+import numpy as np
+from itertools import chain
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+import urllib.request as ur
+from html.parser import HTMLParser
+
+# Setting base URLs and target URLs
+# Defining function all_urls() for grabbing links for all pages on reed's website for the day
+
+base = "http://www.reed.co.uk"
+url = "http://www.reed.co.uk/jobs?datecreatedoffset=Today"
+
+def all_urls():
+    r = requests.get(url).content
+    soup = BeautifulSoup(r, "html.parser")
+    # get the urls from the first page
+    yield  [urljoin(base, a["href"]) for a in soup.select("div.details h3.title a[href^=/jobs]")]
+    nxt = soup.find("a", title="Go to next page")
+    # title="Go to next page" is missing when there are no more pages
+    while nxt:
+        # wash/repeat until no more pages
+        r = requests.get(urljoin(base, nxt["href"])).content
+        soup = BeautifulSoup(r, "html.parser")
+        yield  [urljoin(base, a["href"]) for a in soup.select("div.details h3.title a[href^=/jobs]")]
+        nxt = soup.find("a", title="Go to next page")
+
+# Calling function and extracting links to each vacancy
+
+dflink = pd.DataFrame(columns=["links"], data=list(chain.from_iterable(all_urls())))
+
+# Define function strip_html()
+# Filling out a dataframe with a description + other vars for each vacancy
+
+def strip_html(x):
+    return lxml.html.fromstring(x).text_content() if x else ' '
+
+emptydata = pd.DataFrame({"job_description":[], "salary_disp":[], "salary_min":[], "salary_max":[], "salary_time":[], "job_country":[], "job_region":[], "job_locality":[], "job_postcode":[], "job_type":[], "job_type_disp":[], "applications_ten":[], "link":[]})
+
+for index, row in dflink.iloc[0:dflink.size].iterrows():
+    url = row['links']
+    s = ur.urlopen(url).read()
+    soup = BeautifulSoup(s, "html.parser")
+    description = strip_html(str(soup.find_all("span", itemprop="description")))
+    try:
+        get_a_new_job = pd.DataFrame({
+                "job_description":[description],
+                "salary_disp":[soup.select_one('.salary span').text.strip()],
+                "salary_min":[soup.select_one('.salary meta[itemprop="minValue"]')["content"]],               
+                "salary_max":[soup.select_one('.salary meta[itemprop="maxValue"]')["content"]],
+                "salary_time":[soup.select_one('.salary meta[itemprop="unitText"]')["content"]],               
+                "job_country":[soup.select_one('.location span[id="jobCountry"]')["value"]], 
+                "job_region":[soup.select_one('.location meta[itemprop="addressRegion"]')["content"]],
+                "job_locality":[soup.select_one('.location span[itemprop="addressLocality"]').text],
+                "job_postcode":[soup.select_one('.location meta[itemprop="postalCode"]')["content"]],
+                "job_type":[soup.select_one('.time span[itemprop="employmentType"]')["content"]],
+                "job_type_disp":[soup.select_one('.time span[itemprop="employmentType"]').text],
+                "applications_ten":[soup.select_one('.applications').text.strip()],
+                "link": [row]             
+        })
+        emptydata = emptydata.append(get_a_new_job)
+    except:
+        pass
+
+# Rename dataframe
+reed_data = emptydata
+
+# How many listings have we scraped?
+
+reed_data.size
+
+# Inspect listings as pd frame
+
+reed_data
